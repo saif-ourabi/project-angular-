e2c37fc044ef142f2e4f58ad2d15b5e2a8e65c2c
kitokid <ourabisaif48@gmail.com>
2023-12-25 06:59:47 +0100

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormationListComponent } from './formations/formation-list/formation-list/formation-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormationListComponent,
   ],
   imports: [
     BrowserModule,
